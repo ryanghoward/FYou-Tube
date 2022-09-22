bbc9103f9222cd724d1f42b1b28340bce4ede5cc
Ryan Howard <ryanghoward@protonmail.com>
2022-09-22 14:09:26 -0700

    Fuck this

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import {
+  ChannelDetail,
+  VideoDetail,
+  SearchFeed,
   Navbar,
   Feed,
-  VideoDetail,
-  ChannelDetail,
-  SearchFeed,
 } from "./components";
 
-const App = () => {
+const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: "#000" }}>
       <Navbar />
       <Routes>
-        <Route path='/' exact element={<Feed />} />
+        <Route exact path='/' element={<Feed />} />
         <Route path='/video/:id' element={<VideoDetail />} />
         <Route path='/channel/:id' element={<ChannelDetail />} />
         <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
-  </BrowserRouter>;
-};
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+import { SearchIcon } from "@mui/icons-material";
+
+const SearchBar = () => {
+  return (
+    <Paper component='form' on>
+      SearchBar
+    </Paper>
+  );
+};
+
+export default SearchBar;
